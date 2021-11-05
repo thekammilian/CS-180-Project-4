cd014f5ffe68db6c671c681c4ba8fe5f7f35cb0c
thekammilian <53964761+thekammilian@users.noreply.github.com>
2021-11-05 13:53:16 -0400

    project classes added

---
+++ src/User.java
@@ -0,0 +1,25 @@
+public class User {
+    private String username;
+    private String name;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
